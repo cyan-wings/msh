b1dbc2380114e8fbf67641940d84f9b7a3be03df
Matthew Yeow <matthewyeow.yeow@gmail.com>
2024-06-28 16:34:11 +0800

    Added grammar for parse token

--- srcs/minishell_parse/minishell_parse.c
+++ srcs/minishell_parse/minishell_parse.c
@@ -6,21 +6,39 @@
 /*   By: myeow <myeow@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 17:33:55 by myeow             #+#    #+#             */
-/*   Updated: 2024/06/28 14:48:23 by myeow            ###   ########.fr       */
+/*   Updated: 2024/06/28 16:33:03 by myeow            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	minishell_parse(t_list *token_list)
+/*
+ * Should put in STDERR?
+ */
+static void	minishell_parse_error(void)
+{
+	ft_putendl_fd("Parse Error", 1);
+	return ;
+}
+
+static int	minishell_parse_token(t_list *token_list)
 {
 	t_token	*curr;
 
 	while (token_list)
 	{
 		curr = (t_token *) token_list->content;
-		printf("%d\n", minishell_parse_word(curr));
+		if (!minishell_parse_word(curr) && \
+				!minishell_parse_operator(curr, DEFAULT))
+			return (0);
 		token_list = token_list->next;
 	}
+	return (1);
+}
+
+void	minishell_parse(t_list *token_list)
+{
+	if (!minishell_parse_token(token_list))
+		minishell_parse_error();
 	return ;
 }
