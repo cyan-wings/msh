ceab57ccfdb95bfa62ce8d9ec7cecd92cb10bb54
Matthew Yeow <matthewyeow.yeow@gmail.com>
2024-07-07 14:39:27 +0800

    Fixed norme and minor indent bug in redirections.

--- srcs/minishell_parse/expression/cmd/minishell_parse_cmd_redirections.c
+++ srcs/minishell_parse/expression/cmd/minishell_parse_cmd_redirections.c
@@ -6,7 +6,7 @@
 /*   By: myeow <myeow@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 17:48:03 by myeow             #+#    #+#             */
-/*   Updated: 2024/07/06 22:06:06 by myeow            ###   ########.fr       */
+/*   Updated: 2024/07/07 14:37:03 by myeow            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,10 +45,12 @@ int	minishell_parse_cmd_redirections(t_list **token_ptr,
 	{
 		if (!((*token_ptr)->next) || \
 			((t_token *)(*token_ptr)->next->content)->type != WORD)
+		{
 			minishell_parse_astfree(&redir_child_node);
 			return (0);
+		}
 		redir_child_node = redirection((t_token *)(*token_ptr)->content, \
-						   (t_token *)(*token_ptr)->next->content);
+				(t_token *)(*token_ptr)->next->content);
 		minishell_parse_astadd_child(*redirs_root_node, redir_child_node);
 		minishell_tokenise_get_next_token(token_ptr);
 		minishell_tokenise_get_next_token(token_ptr);
