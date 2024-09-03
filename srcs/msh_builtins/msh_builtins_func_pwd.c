60a6513fd71e77f6085fb93228257af8cff12059
Matthew Yeow <matthewyeow.yeow@gmail.com>
2024-09-03 16:53:22 +0800

    Partially completed cd builtin, introduced restricted envs in unset builtin

--- srcs/msh_builtins/msh_builtins_func_pwd.c
+++ srcs/msh_builtins/msh_builtins_func_pwd.c
@@ -6,7 +6,7 @@
 /*   By: myeow <myeow@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/01 16:55:44 by myeow             #+#    #+#             */
-/*   Updated: 2024/09/02 16:16:54 by myeow            ###   ########.fr       */
+/*   Updated: 2024/09/03 16:39:54 by myeow            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,15 @@
  * But when the 1st argument is given without a "-", it runs as usual
  * without flagging any errors.
  */
-int	msh_builtins_func_pwd(int argc __attribute((unused)), char **argv,
-		t_list **env_list, int subshell_flag __attribute((unused)))
+int	msh_builtins_func_pwd(
+		int argc __attribute((unused)),
+		char **argv,
+		t_list **env_list __attribute((unused)),
+		int subshell_flag __attribute((unused))
+		)
 {
+	char	buffer[PATH_MAX];
+
 	if (argv[1] && argv[1][0] == '-')
 	{
 		ft_putstr_fd("msh: pwd: ", 2);
@@ -32,6 +38,11 @@ int	msh_builtins_func_pwd(int argc __attribute((unused)), char **argv,
 		ft_putendl_fd("pwd: usage: pwd", 2);
 		return (1);
 	}
-	ft_putendl_fd(msh_env_getvar(*env_list, "PWD"), 1);
+	if (!getcwd(buffer, sizeof(buffer)))
+	{
+		ft_putendl_fd("getcwd error.", 2);
+		return (1);
+	}
+	ft_putendl_fd(buffer, 1);
 	return (0);
 }
