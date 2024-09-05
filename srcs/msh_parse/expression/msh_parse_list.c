fc872b30909ecf93ffcc07311943b125c7be5765
Matthew Yeow <matthewyeow.yeow@gmail.com>
2024-09-05 17:21:17 +0800

    Fixed freeing bug where msh_parse_list frees pipeline, would shouldn't. Fixed to flag errors in lst related commands in cases where it doesn't end properly.

--- srcs/msh_parse/expression/msh_parse_list.c
+++ srcs/msh_parse/expression/msh_parse_list.c
@@ -6,7 +6,7 @@
 /*   By: myeow <myeow@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 13:51:30 by myeow             #+#    #+#             */
-/*   Updated: 2024/09/05 16:32:57 by myeow            ###   ########.fr       */
+/*   Updated: 2024/09/05 17:11:51 by myeow            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,12 +35,9 @@ static void	astadd_list_op_node(t_list **token_ptr, t_ast **list_root_node)
  *	(2)	when current is AND or OR token but no token following it.
  *		Requires freeing of both nodes for this scenario.
  */
-t_ast	*msh_parse_list_error(t_ast **list_root_node,
-		t_ast **pipeline_node)
+t_ast	*msh_parse_list_error(t_ast **list_root_node)
 {
 	msh_parse_astfree(list_root_node);
-	if (!*pipeline_node)
-		msh_parse_astfree(pipeline_node);
 	return (0);
 }
 
@@ -54,18 +51,19 @@ t_ast	*msh_parse_list(t_list **token_ptr)
 	pipeline_node = 0;
 	pipeline_node = msh_parse_pipeline(token_ptr);
 	if (!pipeline_node)
-		return (msh_parse_list_error(&list_root_node, &pipeline_node));
+		return (msh_parse_list_error(&list_root_node));
 	msh_parse_astadd_child(list_root_node, pipeline_node);
 	while (*token_ptr && \
 			(!ft_strcmp(((t_token *)(*token_ptr)->content)->value, AND) || \
 			!ft_strcmp(((t_token *)(*token_ptr)->content)->value, OR)))
 	{
-		if (!pipeline_node || !((*token_ptr)->next))
-			return (msh_parse_list_error(&list_root_node, \
-						&pipeline_node));
+		if (!((*token_ptr)->next))
+			return (msh_parse_list_error(&list_root_node));
 		astadd_list_op_node(token_ptr, &list_root_node);
 		msh_tokenise_get_next_token(token_ptr);
 		pipeline_node = msh_parse_pipeline(token_ptr);
+		if (!pipeline_node)
+			return (msh_parse_list_error(&list_root_node));
 		msh_parse_astadd_child(list_root_node, pipeline_node);
 	}
 	return (list_root_node);
